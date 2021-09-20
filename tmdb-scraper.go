23c94360312e7269d2c97802b25db39d757e044f
Sander Lambrechts <sander-lambrechts@outlook.com>
2021-09-20 12:21:56 +0200

    configParser renamed to parseConfig. Moved functions to above the main function. Removed some spaces behind the :. Added some notes about what code is doing Removed initializing the pagina var as int64 and specify directly int64 in for loop Added for loop to switch between pages, so if wrong enter than stay in for loop to ask again. Only break if output is correct.

--- tmdb-scraper.go
+++ tmdb-scraper.go
@@ -17,7 +17,7 @@ type Config struct {
 	Api string `yaml:"api-key"` // Eerst de config.Api specificeren, dan wat de variabele is (string) daarna inladen
 }
 
-func configParser(configFile string) Config {
+func parseConfig(configFile string) Config {
 	// YML-parser voor de config
 	filename, _ := filepath.Abs(configFile)
 	yamlFile, err := ioutil.ReadFile(filename)
@@ -27,111 +27,6 @@ func configParser(configFile string) Config {
 		panic(err)
 	}
 	return config
-}
-
-func main() {
-	// Instellen van error code prefix en verberg de tijdstempel
-	log.SetPrefix("Foutmelding: ")
-	log.SetFlags(0)
-
-	// Laad de config file in de config parser functie en zet de return in de variabele config
-	config := configParser("settings.yml")
-
-	// API key TMDB als environment opslaan in err en als err niet nul dan terug
-	err := os.Setenv("APIKey", config.Api)
-	if err != nil {
-		return
-	}
-
-	// Print tekst
-	fmt.Println("Zoek naar films/series/acteurs: ")
-
-	// Input voor zoekopdracht (met Bufio is het mogelijk voor spaties)
-	input := bufio.NewReader(os.Stdin)
-	text, _ := input.ReadString('\n')
-
-	if text == "\n" {
-		log.Fatal("U moet een zoekopdracht invoeren!")
-	}
-
-	zoekopdracht := search(text, 1)
-
-	var pagina int64
-
-	for pagina = 1; pagina <= zoekopdracht.TotalPages; {
-		if zoekopdracht.TotalPages == 1 {
-			fmt.Println("Voer het nummer in van de rij die je wilt kiezen:")
-			var input string
-			_, err = fmt.Scanln(&input)
-			if err != nil {
-				return
-			}
-			if input == "1" {
-				fmt.Println("Nummer 1 gekozen!")
-				return
-			} else {
-				return
-			}
-		} else if zoekopdracht.TotalPages > 1 && int(zoekopdracht.TotalPages) != zoekopdracht.Page {
-			if zoekopdracht.Page == 1 {
-				fmt.Println("Kies: 1 voor huidige pagina - 2 voor volgende pagina")
-
-				var input string
-				_, err = fmt.Scanln(&input)
-				if err != nil {
-					return
-				}
-				if input == "1" {
-					fmt.Println("Nummer 1 gekozen!")
-					return
-				} else if input == "2" {
-					pagina++
-					zoekopdracht = search(text, int(pagina))
-				} else {
-					fmt.Println("U moet getal 1 of 2 invoeren!")
-				}
-			} else {
-				fmt.Println("Kies: 0 voor vorige pagina - 1 voor huidige pagina - 2 voor volgende pagina")
-
-				var input string
-				_, err = fmt.Scanln(&input)
-				if err != nil {
-					return
-				}
-				if input == "0" {
-					pagina--
-					zoekopdracht = search(text, int(pagina))
-				} else if input == "1" {
-					fmt.Println("Nummer 1 gekozen!")
-					return
-				} else if input == "2" {
-					pagina++
-					zoekopdracht = search(text, int(pagina))
-				} else {
-					fmt.Println("U moet getal 0, 1 of 2 invoeren!")
-				}
-			}
-
-		} else if int64(zoekopdracht.Page) == zoekopdracht.TotalPages {
-			fmt.Println("Kies: 0 voor vorige pagina - 1 voor huidige pagina")
-			var input string
-			_, err = fmt.Scanln(&input)
-			if err != nil {
-				return
-			}
-			if input == "0" {
-				pagina--
-				zoekopdracht = search(text, int(pagina))
-			} else if input == "1" {
-				fmt.Println("Nummer 1 gekozen!")
-				return
-			} else {
-				fmt.Println("U moet getal 0 of 1 invoeren!")
-				return
-			}
-		}
-	}
-
 }
 
 func search(input string, pagina int) *tmdb.SearchMulti {
@@ -161,14 +56,136 @@ func search(input string, pagina int) *tmdb.SearchMulti {
 	fmt.Println("Resultaten - Pagina", zoekopdracht.Page, "-", zoekopdracht.TotalPages, ":")
 	for i, v := range zoekopdracht.Results {
 		if v.MediaType == "movie" {
-			fmt.Println(i+1, "- Film: ", v.Title)
+			fmt.Println(i+1, "- Film:", v.Title)
 		} else if v.MediaType == "tv" {
-			fmt.Println(i+1, "- Serie: ", v.Name)
+			fmt.Println(i+1, "- Serie:", v.Name)
 		} else if v.MediaType == "person" {
-			fmt.Println(i+1, "- Persoon: ", v.Name)
+			fmt.Println(i+1, "- Persoon:", v.Name)
 		}
 	}
 	fmt.Println("=====")
 
 	return zoekopdracht
+}
+
+func main() {
+	// Instellen van error code prefix en verberg de tijdstempel
+	log.SetPrefix("Foutmelding: ")
+	log.SetFlags(0)
+
+	// Laad de config file in de config parser functie en zet de return in de variabele config
+	config := parseConfig("settings.yml")
+
+	// API key TMDB als environment opslaan in err en als err niet nul dan terug
+	err := os.Setenv("APIKey", config.Api)
+	if err != nil {
+		return
+	}
+
+	// Print tekst
+	fmt.Println("Zoek naar films/series/acteurs:")
+
+	// Input voor zoekopdracht (met Bufio is het mogelijk voor spaties)
+	input := bufio.NewReader(os.Stdin)
+	text, _ := input.ReadString('\n')
+
+	if text == "\n" {
+		log.Fatal("U moet een zoekopdracht invoeren!")
+	}
+
+	// Krijg de waarden terug van de zoekopdracht
+	zoekopdracht := search(text, 1)
+
+	// Voor elke pagina, tussen 1 en totaal aantal van de zoekopdracht doe het volgende
+	for pagina := int64(1); pagina <= zoekopdracht.TotalPages; {
+		// Als de zoekopdracht 1 pagina heeft dan:
+		if zoekopdracht.TotalPages == 1 {
+			fmt.Println("Voer het nummer in van de rij die je wilt kiezen:")
+
+			// Loop voor als er iets anders gekozen wordt
+			for {
+				var input string
+				_, err = fmt.Scanln(&input)
+
+				// Als er een error wordt gegenereerd
+				if err != nil {
+					fmt.Println("Voer een getal in!")
+				} else if input == "1" {
+					fmt.Println("Nummer 1 gekozen!")
+					return
+				} else {
+					fmt.Println("Voer een getal in!")
+				}
+			}
+			//Als totaal pagina's groter is dan 1 en het totaal aantal pagina's niet overeenkomt met de huidige pagina
+		} else if zoekopdracht.TotalPages > 1 && int(zoekopdracht.TotalPages) != zoekopdracht.Page {
+			// Als je op de eerste pagina bent dan
+			if zoekopdracht.Page == 1 {
+				fmt.Println("Kies: 1 voor huidige pagina - 2 voor volgende pagina")
+
+				for {
+					var input string
+					_, err = fmt.Scanln(&input)
+
+					if err != nil {
+						fmt.Println("Graag 1 of 2 invoeren!")
+					} else if input == "1" {
+						fmt.Println("Nummer 1 gekozen!")
+						return
+					} else if input == "2" {
+						pagina++
+						zoekopdracht = search(text, int(pagina))
+						break
+					} else {
+						fmt.Println("Graag 1 of 2 invoeren!")
+					}
+				}
+			} else {
+				fmt.Println("Kies: 0 voor vorige pagina - 1 voor huidige pagina - 2 voor volgende pagina")
+
+				for {
+					var input string
+					_, err = fmt.Scanln(&input)
+
+					if err != nil {
+						fmt.Println("Graag 0, 1 of 2 invoeren!")
+					} else if input == "0" {
+						pagina--
+						zoekopdracht = search(text, int(pagina))
+						break
+					} else if input == "1" {
+						fmt.Println("Nummer 1 gekozen!")
+						return
+					} else if input == "2" {
+						pagina++
+						zoekopdracht = search(text, int(pagina))
+						break
+					} else {
+						fmt.Println("Graag 0, 1 of 2 invoeren!")
+					}
+				}
+			}
+			// Als de pagina overeenkomt met de laatste pagina dan
+		} else if int64(zoekopdracht.Page) == zoekopdracht.TotalPages {
+			fmt.Println("Kies: 0 voor vorige pagina - 1 voor huidige pagina")
+
+			for {
+				var input string
+				_, err = fmt.Scanln(&input)
+
+				if err != nil {
+					fmt.Println("Graag 0 of 1 invoeren!")
+				} else if input == "0" {
+					pagina--
+					zoekopdracht = search(text, int(pagina))
+					break
+				} else if input == "1" {
+					fmt.Println("Nummer 1 gekozen!")
+					return
+				} else {
+					fmt.Println("Graag 0 of 1 invoeren!")
+				}
+			}
+		}
+	}
 }
